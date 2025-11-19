44c7616c9d10fa91382073cf5a940d5034631047
Brian S. Memusi <bsmemusi@gmail.com>
2025-11-19 22:58:31 +0300

    Delete fedhasmart-main directory

--- fedhasmart-main/supabase/migrations/DeleteProfile.sql
+++
@@ -1,5 +0,0 @@
--- Add DELETE policy for profiles table to allow users to delete their own data
-CREATE POLICY "Users can delete own profile" 
-ON public.profiles 
-FOR DELETE 
-USING (auth.uid() = id);
